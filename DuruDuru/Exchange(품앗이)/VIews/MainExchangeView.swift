e3f0cff1b0e20e28d76322cd8215c896666d1ace
hyojin425 <gywls5184@gmail.com>
2025-01-27 20:02:28 +0900

    [FEAT] 메인 품앗이 화면 상단바에 버튼 추가

--- DuruDuru/Exchange(품앗이)/VIews/MainExchangeView.swift
+++ DuruDuru/Exchange(품앗이)/VIews/MainExchangeView.swift
@@ -24,6 +24,36 @@ class MainExchangeView: UIView {
     
     // MARK: - Components
     
+    /// 주소 바꾸기 버튼
+    private let locationButton = UIButton().then {
+        let configuration = UIButton.Configuration.plain()
+        $0.configuration = configuration
+        $0.configuration?.image = UIImage(named: "underIcon")
+        $0.configuration?.imagePlacement = .trailing
+        $0.configuration?.imagePadding = 5
+        $0.configuration?.baseForegroundColor = .black
+        $0.configuration?.attributedTitle = AttributedString("공릉동", attributes: AttributeContainer([.font: UIFont.boldSystemFont(ofSize: 16)]))
+        $0.configuration?.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+    }
+    
+    /// 프로필 버튼
+    private let profileButton = UIButton().then {
+        $0.setImage(UIImage(named: "profile")?.withRenderingMode(.alwaysTemplate), for: .normal)
+        $0.tintColor = .black
+    }
+    
+    /// 검색 버튼
+    private let searchButton = UIButton().then {
+        $0.setImage(UIImage(named: "search")?.withRenderingMode(.alwaysTemplate), for: .normal)
+        $0.tintColor = .black
+    }
+    
+    /// 알람 버튼
+    private let alarmButton = UIButton().then {
+        $0.setImage(UIImage(named: "alarm")?.withRenderingMode(.alwaysTemplate), for: .normal)
+        $0.tintColor = .black
+    }
+    
     /// 세그먼트
     let segmentedControl = UISegmentedControl(items: ["품앗이", "함께 먹자"]).then {
         $0.setBackgroundImage(UIImage(), for: .normal, barMetrics: .default)
@@ -58,6 +88,10 @@ class MainExchangeView: UIView {
     
     /// 컴포넌트 생성
     private func addComponents() {
+        addSubview(locationButton)
+        addSubview(profileButton)
+        addSubview(searchButton)
+        addSubview(alarmButton)
         addSubview(segmentedControl)
         addSubview(underline)
         addSubview(containerView)
@@ -65,8 +99,28 @@ class MainExchangeView: UIView {
     
     /// 오토레이아웃 설정
     private func constraints() {
+        locationButton.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(54)
+            $0.left.equalToSuperview().offset(16)
+        }
+        
+        alarmButton.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(54)
+            $0.right.equalToSuperview().offset(-16)
+        }
+        
+        searchButton.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(54)
+            $0.right.equalTo(alarmButton.snp.left).offset(-16)
+        }
+        
+        profileButton.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(54)
+            $0.right.equalTo(searchButton.snp.left).offset(-16)
+        }
+        
         segmentedControl.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(75)
+            $0.top.equalTo(locationButton.snp.bottom)
             $0.centerX.equalToSuperview()
             $0.width.equalToSuperview()
             $0.height.equalTo(48)

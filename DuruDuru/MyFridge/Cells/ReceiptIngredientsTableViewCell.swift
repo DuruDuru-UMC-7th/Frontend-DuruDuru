307ff605e307bf78d0f1a34a27e5aa06d2ae264d
hyojin425 <gywls5184@gmail.com>
2025-02-02 04:16:50 +0900

    [FEAT] 영수증 추가 뷰에서 식재료 편집 클릭시 편집 기능 추가

--- DuruDuru/MyFridge/Cells/ReceiptIngredientsTableViewCell.swift
+++ DuruDuru/MyFridge/Cells/ReceiptIngredientsTableViewCell.swift
@@ -12,23 +12,33 @@ class ReceiptIngredientsTableViewCell: UITableViewCell {
     // MARK: - Init
     
     static let identifier: String = "ReceiptIngredientsTableViewCell"
-
+    weak var delegate: DeleteButtonDelegate?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         self.backgroundColor = .white
+        
+        selectionStyle = .none
+        self.clipsToBounds = false
+        self.contentView.clipsToBounds = false
+        
         addComponents()
         constraints()
+        
+        self.decrementButton.addTarget(self, action: #selector(decrementButtonClicked), for: .touchUpInside)
+        self.incrementButton.addTarget(self, action: #selector(incrementButtonClicked), for: .touchUpInside)
+        self.deleteButton.addTarget(self, action: #selector(deleteButtonClicked), for: .touchUpInside)
     }
     
     required init?(coder: NSCoder) {
@@ -46,7 +56,7 @@ class ReceiptIngredientsTableViewCell: UITableViewCell {
         $0.backgroundColor = UIColor(hex: 0xF4F4F5)
         $0.layer.cornerRadius = 10
     }
-
+    
     let ingredientName = UILabel().then {
         $0.font = .boldSystemFont(ofSize: 13)
         $0.textColor = UIColor(hex: 0x2E2F33, alpha: 0.88)
@@ -55,7 +65,7 @@ class ReceiptIngredientsTableViewCell: UITableViewCell {
     let count = UILabel().then {
         $0.text = "X1"
         $0.layer.cornerRadius = 4
-        $0.layer.masksToBounds = true 
+        $0.layer.masksToBounds = true
         $0.backgroundColor = .white
         $0.font = .boldSystemFont(ofSize: 14)
         $0.textColor = UIColor(hex: 0x171719)
@@ -66,6 +76,35 @@ class ReceiptIngredientsTableViewCell: UITableViewCell {
         $0.backgroundColor = .white
     }
     
+    let decrementButton = UIButton().then {
+        $0.setImage(.decrementButton, for: .normal)
+        $0.layer.cornerRadius = 8.91
+        $0.backgroundColor = .white
+        $0.isHidden = true
+    }
+    
+    let countLabel = UILabel().then {
+        $0.text = "1"
+        $0.textAlignment = .center
+        $0.font = .systemFont(ofSize: 14)
+        $0.isHidden = true
+    }
+    
+    let incrementButton = UIButton().then {
+        $0.setImage(.incrementButton, for: .normal)
+        $0.layer.cornerRadius = 8.91
+        $0.backgroundColor = .white
+        $0.isHidden = true
+    }
+    
+    let deleteButton = UIButton().then {
+        $0.setImage(.trash, for: .normal)
+        $0.layer.cornerRadius = 4
+        $0.backgroundColor = UIColor(hex: 0xD9D9D9)
+        $0.tintColor = .white
+        $0.isHidden = true
+    }
+    
     // MARK: - Constaints & Add Function
     
     /// 컴포넌트 생성
@@ -74,6 +113,11 @@ class ReceiptIngredientsTableViewCell: UITableViewCell {
         addSubview(marginView)
         containView.addSubview(ingredientName)
         containView.addSubview(count)
+        
+        containView.addSubview(decrementButton)
+        containView.addSubview(countLabel)
+        containView.addSubview(incrementButton)
+        containView.addSubview(deleteButton)
     }
     
     /// 오토레이아웃 설정
@@ -101,11 +145,68 @@ class ReceiptIngredientsTableViewCell: UITableViewCell {
             $0.left.right.equalToSuperview()
             $0.height.equalTo(10)
         }
+        
+        deleteButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.right.equalToSuperview().offset(-11)
+            $0.width.height.equalTo(28)
+        }
+        
+        incrementButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.right.equalTo(deleteButton.snp.left).offset(-11)
+            $0.width.height.equalTo(26)
+        }
+        
+        countLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.width.equalTo(14)
+            $0.right.equalTo(incrementButton.snp.left).offset(-7)
+        }
+        
+        decrementButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.right.equalTo(countLabel.snp.left).offset(-7)
+            $0.width.height.equalTo(26)
+        }
     }
     
-    public func configure(ingredient: ReceiptIngredients) {
+    public func configure(ingredient: ReceiptIngredients, isEditing: Bool) {
         ingredientName.text = ingredient.ingredientName
         count.text = "x" + String(ingredient.count)
+        countLabel.text = String(ingredient.count)
         
+        decrementButton.isHidden = !isEditing
+        countLabel.isHidden = !isEditing
+        incrementButton.isHidden = !isEditing
+        deleteButton.isHidden = !isEditing
+        count.isHidden = isEditing
     }
+    
+    @objc func decrementButtonClicked() {
+        guard let currentCount = Int(countLabel.text ?? "0"), currentCount > 1 else { return }
+        
+        let newCount = currentCount - 1
+        countLabel.text = "\(newCount)"
+        
+        delegate?.didUpdateCount(in: self, newCount: newCount)
+    }
+
+    @objc func incrementButtonClicked() {
+        guard let currentCount = Int(countLabel.text ?? "0") else { return }
+        
+        let newCount = currentCount + 1
+        countLabel.text = "\(newCount)"
+        
+        delegate?.didUpdateCount(in: self, newCount: newCount)
+    }
+    
+    @objc func deleteButtonClicked() {
+        delegate?.didTapDeleteButton(in: self)
+    }
+}
+
+protocol DeleteButtonDelegate: AnyObject {
+    func didTapDeleteButton(in cell: ReceiptIngredientsTableViewCell)
+    func didUpdateCount(in cell: ReceiptIngredientsTableViewCell, newCount: Int)
 }

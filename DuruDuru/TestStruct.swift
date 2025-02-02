49e21a3b03242203c9eac8b9bceca212844237ad
guaugua <evan24865@naver.com>
2025-02-02 14:34:32 +0900

    깃 충돌 테스트

---
+++ DuruDuru/TestStruct.swift
@@ -0,0 +1,16 @@
+//
+//  TestStruct.swift
+//  DuruDuru
+//
+//  Created by 이은찬 on 2/2/25.
+//
+
+import Foundation
+
+struct TestStruct {
+    let name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}

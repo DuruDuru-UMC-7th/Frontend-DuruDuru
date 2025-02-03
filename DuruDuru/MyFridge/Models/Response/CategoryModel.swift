8f0ec2084e211340686d24d5359b846659e3739f
윤시진 <sireal@yunsijin-ui-MacBookAir.local>
2025-02-03 23:38:59 +0900

    대분류에 속하는 소분류 카테고리 조회 api 구현

---
+++ DuruDuru/MyFridge/Models/Response/CategoryModel.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryModel.swift
+//  DuruDuru
+//
+//  Created by 윤시진 on 2/3/25.
+//
+
+import UIKit
+
+struct CategoryResponse: Codable {
+    let isSuccess: Bool
+    let code: String
+    let message: String
+    let result: CategoryResult
+}
+
+struct CategoryResult: Codable {
+    let majorCategory: String
+    let minorCategoryList: [String]
+}
--- DuruDuru/Service/APIClient.swift
+++ DuruDuru/Service/APIClient.swift
@@ -13,7 +13,7 @@ final class APIClient {
     private let session: Session
     
     private init() {
-//        let interceptor = AuthorizationInterceptor(accessToken: "eyJhbGciOiJIUzI1NiJ9.eyJpZCI6NSwiaWF0IjoxNzM4NDk0OTc4LCJleHAiOjE3Mzg0OTg1Nzh9.bj3DkGLnDRtWHgvUvH3IQJePUbiWgoeeo2iOxtd6Vf8")
+       let interceptor = AuthorizationInterceptor(accessToken: "eeyJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwiaWF0IjoxNzM4NTkxNjU3LCJleHAiOjE3Mzg1OTUyNTd9.quaNtWYfSF_n9kxl6OwXMyehtk0vKmvMVEts50b2H3A")
         session = Session()
     }
     
@@ -70,4 +70,41 @@ final class APIClient {
             }
         }
     }
+    
+    /// 대분류에 따른 소분류 조회
+    public func getIngredientCategories(
+        majorCategory: String,
+        completion: @escaping (Result<[String], Error>) -> Void
+    ) {
+        let baseUrl = "http://3.35.252.162:8080/ingredient/category/major-to-minor"
+        
+        let urlWithQuery = "\(baseUrl)?majorCategory=\(majorCategory.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? "")"
+
+        print("Request URL: \(urlWithQuery)") // 디버깅용 출력
+
+        session.request(urlWithQuery, method: .get)
+            .validate()
+            .responseDecodable(of: CategoryResponse.self) { response in
+                switch response.result {
+                case .success(let data):
+                    if data.isSuccess {
+                        completion(.success(data.result.minorCategoryList))
+                    } else {
+                        let error = NSError(domain: "", code: 400, userInfo: [NSLocalizedDescriptionKey: data.message])
+                        completion(.failure(error))
+                    }
+                case .failure(let error):
+                    print("오류 발생: \(error.localizedDescription)")
+                    if let httpResponse = response.response {
+                        print("Status Code: \(httpResponse.statusCode)")
+                    }
+                    if let data = response.data,
+                       let errorMessage = String(data: data, encoding: .utf8) {
+                        print("Response Data: \(errorMessage)")
+                    }
+                    completion(.failure(error))
+                }
+            }
+    }
+
 }

09078ace5ff8672c94499a580e01278bee31af24
hyojin425 <gywls5184@gmail.com>
2025-02-19 15:59:26 +0900

    [FEAT] accessToken 헤더에 반영 및 accessToken 재발급 API 연결

--- DuruDuru/Service/AuthorizationInterceptor.swift
+++ DuruDuru/Service/AuthorizationInterceptor.swift
@@ -9,7 +9,7 @@ import Foundation
 import Alamofire
 
 class AuthorizationInterceptor: RequestInterceptor {
-    private let accessToken: String
+    private var accessToken: String
     
     init(accessToken: String) {
         self.accessToken = accessToken
@@ -21,8 +21,34 @@ class AuthorizationInterceptor: RequestInterceptor {
         completion(.success(request))
     }
     
-    func retry(_ request: Request, for session: Session, dueTo error: any Error, completion: @escaping (RetryResult) -> Void) {
-        completion(.doNotRetry)
+    func retry(_ request: Request, for session: Session, dueTo error: Error, completion: @escaping (RetryResult) -> Void) {
+        guard let response = request.task?.response as? HTTPURLResponse else {
+            completion(.doNotRetryWithError(error))
+            return
+        }
+        
+        switch response.statusCode {
+        case 401: // Unauthorized
+            AuthAPI.shared.refreshAuthentication { result in
+                switch result {
+                case .success(let refreshResponse):
+                    // 새로운 accessToken 저장
+                    TokenSave.shared.accessToken = refreshResponse.result.accessToken
+                    TokenSave.shared.accessToken = refreshResponse.result.refreshToken
+                    
+                    // AuthorizationInterceptor 업데이트
+                    let interceptor = AuthorizationInterceptor(accessToken: TokenSave.shared.accessToken!)
+                    APIClient.shared.updateAuthorizationToken(interceptor: interceptor)
+                    
+                    print("Access Token이 성공적으로 갱신되었습니다.")
+                    
+                case .failure(let error):
+                    print("Error refreshing token: \(error.localizedDescription)")
+                }
+            }
+        default:
+            completion(.doNotRetry)
+        }
     }
     
 }

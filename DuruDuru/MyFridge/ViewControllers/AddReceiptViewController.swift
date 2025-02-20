34696b79720e845208a3e4d5ee5abb92113c1c2d
hyojin425 <gywls5184@gmail.com>
2025-02-20 20:07:27 +0900

    [FIX] OCR 영수증 인식 버그 수정

--- DuruDuru/MyFridge/ViewControllers/AddReceiptViewController.swift
+++ DuruDuru/MyFridge/ViewControllers/AddReceiptViewController.swift
@@ -30,24 +30,24 @@ class AddReceiptViewController: UIViewController {
     
     // MARK: - Functions
     
-    private func startScanning() {
-        addReceiptView.animateScanningBar()
-        addReceiptView.indicator.startAnimating()
-        self.OCRScan(image: self.image ?? UIImage()) 
-        
-        /// 3초 후 스캔 완료
-        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-            self.addReceiptView.indicator.isHidden = true
-            self.addReceiptView.completeIcon.isHidden = false
-            self.addReceiptView.glowView.isHidden = true
-            self.addReceiptView.scanLabel.text = "인식 완료!"
-            
-            /// 인식 완료 화면 2초 보여주고 화면 전환
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                self.scanningCompleted(receipt: self.receipt)
-            }
-        }
-    }
+//    private func startScanning() {
+//        addReceiptView.animateScanningBar()
+//        addReceiptView.indicator.startAnimating()
+//        self.OCRScan(image: self.image ?? UIImage()) 
+//        
+//        /// 3초 후 스캔 완료
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+//            self.addReceiptView.indicator.isHidden = true
+//            self.addReceiptView.completeIcon.isHidden = false
+//            self.addReceiptView.glowView.isHidden = true
+//            self.addReceiptView.scanLabel.text = "인식 완료!"
+//            
+//            /// 인식 완료 화면 2초 보여주고 화면 전환
+//            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+//                self.scanningCompleted(receipt: self.receipt)
+//            }
+//        }
+//    }
  
     private func scanningCompleted(receipt: ReceiptResult) {
         addReceiptView.stopScanningBarAnimation()
@@ -66,24 +66,75 @@ class AddReceiptViewController: UIViewController {
     
     // MARK: - API 관련
     
-    /// OCR로 영수증 인식 API 연결
+//    /// OCR로 영수증 인식 API 연결
+//    private func OCRScan(image: UIImage) {
+//        let url = "http://3.35.252.162:8080/OCR/receipt"
+//        
+//        // 이미지 데이터로 변환
+//        guard let imageData = image.pngData() else {
+//            print("이미지 변환 실패")
+//            return
+//        }
+//        
+//        // multipart/form-data 요청
+//        APIClient.shared.upload(url: url, imageData: imageData, name: "file") { (result: Result<ReceiptResponse, Error>) in
+//            switch result {
+//            case .success(let response):
+//                print("영수증 등록 성공")
+//                self.receipt = response.result
+//            case .failure(let error):
+//                print("네트워킹 오류: \(error)")
+//            }
+//        }
+//    }
+    
+    private func startScanning() {
+        addReceiptView.animateScanningBar()
+        addReceiptView.indicator.startAnimating()
+        
+        // 이미지가 nil일 경우를 대비하여 안전하게 처리
+        guard let scannedImage = self.image else {
+            print("이미지가 존재하지 않습니다.")
+            return
+        }
+        
+        self.OCRScan(image: scannedImage)
+    }
+
     private func OCRScan(image: UIImage) {
         let url = "http://3.35.252.162:8080/OCR/receipt"
         
-        // 이미지 데이터로 변환
         guard let imageData = image.pngData() else {
             print("이미지 변환 실패")
             return
         }
         
-        // multipart/form-data 요청
         APIClient.shared.upload(url: url, imageData: imageData, name: "file") { (result: Result<ReceiptResponse, Error>) in
             switch result {
             case .success(let response):
                 print("영수증 등록 성공")
                 self.receipt = response.result
+                
+                // 스캔 완료 후 UI 업데이트
+                DispatchQueue.main.async {
+                    self.addReceiptView.indicator.isHidden = true
+                    self.addReceiptView.completeIcon.isHidden = false
+                    self.addReceiptView.glowView.isHidden = true
+                    self.addReceiptView.scanLabel.text = "인식 완료!"
+                    
+                    // 인식 완료 화면 2초 보여주고 화면 전환
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                        self.scanningCompleted(receipt: self.receipt)
+                    }
+                }
             case .failure(let error):
-                print("네트워킹 오류: \(error)")
+                print("네트워킹 오류: \(error.localizedDescription)")
+                // 오류 처리를 위한 UI 업데이트
+                DispatchQueue.main.async {
+                    self.addReceiptView.indicator.isHidden = true
+                    self.addReceiptView.scanLabel.text = "인식 실패"
+                    // 필요에 따라 다른 UI 처리를 추가할 수 있음
+                }
             }
         }
     }
